6bcecc6bc0292098a94865be161c96ab089fe591
Shaktishankar Acharya <86561217+ssacharya2002@users.noreply.github.com>
2025-01-20 11:34:26 +0530

    cors added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import profileRoutes from './routes/profileRoutes';
 import { notFound, errorHandler } from './middleware/errorMiddleware';
-
+import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use('/api/profiles', profileRoutes);
